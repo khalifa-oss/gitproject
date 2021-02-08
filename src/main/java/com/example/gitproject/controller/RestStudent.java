a911b6908cc37ccc0458029c179531d754b8d49c
GAFSA\khalifa saidi <khalifa.saidi@cpg.com.tn>
2021-02-08 14:49:19 +0100

    commit6

--- src/main/java/com/example/gitproject/controller/RestStudent.java
+++ src/main/java/com/example/gitproject/controller/RestStudent.java
@@ -1,7 +1,23 @@
 package com.example.gitproject.controller;
 
 import com.example.gitproject.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.gitproject.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/student")
 public class RestStudent {
+    @Autowired
+    public StudentService studentService;
+
+    @GetMapping("/get/{id}")
+    public Optional<Student> getone(@PathVariable int id) {
+        return studentService.get(id);
+    }
 }
