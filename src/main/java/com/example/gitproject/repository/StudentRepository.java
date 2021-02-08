37a5172fc347a7dd1b9efcbfd4a71759b3f78ffd
GAFSA\khalifa saidi <khalifa.saidi@cpg.com.tn>
2021-02-08 14:42:41 +0100

    commit5

--- src/main/java/com/example/gitproject/repository/StudentRepository.java
+++ src/main/java/com/example/gitproject/repository/StudentRepository.java
@@ -1,7 +1,12 @@
 package com.example.gitproject.repository;
 
 import com.example.gitproject.model.Student;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StudentRepository extends JpaRepository<Student,Integer> {
+
+    Optional<Student> findById(int id);
 }
--- src/main/java/com/example/gitproject/service/StudentService.java
+++ src/main/java/com/example/gitproject/service/StudentService.java
@@ -1,4 +1,19 @@
 package com.example.gitproject.service;
 
+import com.example.gitproject.model.Student;
+import com.example.gitproject.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
+@Transactional
 public class StudentService {
+    @Autowired
+    public StudentRepository studentRepository;
+    public Optional<Student> get(int id){
+       return( studentRepository.findById(id));
+    }
 }
