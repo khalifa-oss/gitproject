81a4db459e43401d16bd709fa8a11fda17a0cf83
GAFSA\khalifa saidi <khalifa.saidi@cpg.com.tn>
2021-02-08 11:11:45 +0100

    commit 3

--- src/main/java/com/example/gitproject/model/Student.java
+++ src/main/java/com/example/gitproject/model/Student.java
@@ -1,4 +1,18 @@
 package com.example.gitproject.model;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import static javax.persistence.GenerationType.SEQUENCE;
+
+@Entity
+@Data
 public class Student {
+    @Id
+    @GeneratedValue(strategy = SEQUENCE)
+    int id;
 }
